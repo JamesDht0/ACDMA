fixed mistake in normalizing. previous calculations in r is wrong since it was normalized by coordinates. instead, it should be normalized by displacement.

data.py
@@ -116,7 +116,7 @@ def final_x_T(directory):
         final_z.append(data[i].trajectory[-1,1])
         T.append(data[i].T)
 
-    order  = np.argsort(T)
+    order = np.argsort(T)
     final_r_sorted = np.array(final_r)[order]
     final_z_sorted = np.array(final_z)[order]
     T_sorted = np.array(T)[order]
@@ -236,8 +236,8 @@ def compare_final_x_minus_dc(directory, variable_dict, minus_DC = True, normaliz
             DC_final_z = DCdata.trajectory[-1, 1]
             if normalizeDC:
                 for d in data:
-                    final_r.append(d.trajectory[-1, 0]/DC_final_r - 1)
-                    final_z.append(d.trajectory[-1, 1]/DC_final_z - 1)
+                    final_r.append((d.trajectory[-1, 0]-x0[0])/(DC_final_r-x0[0]) - 1)
+                    final_z.append((d.trajectory[-1, 1]-x0[1])/(DC_final_z-x0[1]) - 1)
                     T.append(d.T)
             else:
                 for d in data:
@@ -251,8 +251,8 @@ def compare_final_x_minus_dc(directory, variable_dict, minus_DC = True, normaliz
                 DC_final_r = DCdata.trajectory[-1, 0]
                 DC_final_z = DCdata.trajectory[-1, 1]
                 for d in data:
-                    final_r.append(d.trajectory[-1, 0]/DC_final_r)
-                    final_z.append(d.trajectory[-1, 1]/DC_final_z)
+                    final_r.append((d.trajectory[-1, 0]-x0[0])/(DC_final_r-x0[0]))
+                    final_z.append((d.trajectory[-1, 1]-x0[1])/(DC_final_z-x0[1]))
                     T.append(d.T)
             else:
                 for d in data:

functions.py
@@ -28,14 +28,14 @@ def _05_plus_sin(V0,T,t,phi):
 
     if T != 0:
         phase = 2 * math.pi * t / T + phi
-        return -V0*2*(1/2+np.sin(phase))
+        return -V0*(1+2*np.sin(phase))
     else:
         return -V0
 def N_plus_sin(V0,T,t,phi):
-    N = 2
+    N = 3
     if T != 0:
         phase = 2 * math.pi * t / T + phi
-        return -V0*(1+N*np.sin(phase))
+        return -V0*(1+(1/N)*np.sin(phase))
     else:
         return -V0
 
@@ -85,6 +85,14 @@ def linear(x):
     v0 = 2
     # V0 is the velocity at r = 0
     return np.array([0,v0+gradv*x[0]])
+
+def linear_offset(x):
+    gradv = -100
+    v0 = 2
+    v_offset = 1
+    # V0 is the velocity at r = 0
+    return np.array([0,v_offset+v0+gradv*x[0]])
+
 def const(x):
     return np.array([0,1])
 
@@ -100,7 +108,8 @@ waveform_functions = {
 
 flowfield_functions = {
     'linear': linear,
-    'const': const
+    'const': const,
+    'linear_offset' : linear_offset
 }
 
 

main.py
@@ -9,15 +9,15 @@ import matplotlib.pyplot as plt
 
 parameters1 = {
     "rhoP" : [800,2000],
-    "dP" : [1e-6,5e-7,3e-7],
+    "dP" : [1e-6],
     "phi" : [0],
-    "V0" : [1e3],
+    "V0" : [1000],
     #"T" : np.linspace(1e-4, 10e-4, 100).tolist()
-    "T" : sorted(np.logspace(-6, -3, num=127, base=10).tolist() + [0.00])
+    "T" : sorted(np.logspace(-6, -3, num=7, base=10).tolist() + [0.00])
 }
 
 parameters2 = {
-    "rhoP": 1000,
+    "rhoP": 10000,
     "dP": 1e-5,
     "phi": 0,
     "V0": 1e4,
@@ -25,8 +25,8 @@ parameters2 = {
 }
 
 
-waveform = functions.N_plus_sin
-flowfield = functions.linear
+waveform = functions.one_plus_sin
+flowfield = functions.linear_offset
 dt = 1e-7
 n_steps = int(1e6)
 # 40s for one particle running 1e6 steps
@@ -53,6 +53,8 @@ if __name__ == '__main__':
     directory5 = 'raw/wf_half_sawtooth_ff_linear_dt_1e-07_nsteps_1000000_x0_[0.01, 0]_u0_[0, 0]'
     directory6 = 'raw/wf__05_plus_sin_ff_linear_dt_1e-07_nsteps_1000000_x0_[0.01, 0]_u0_[0, 0]'
     directory7 = 'raw/wf_N_plus_sin_ff_linear_dt_1e-07_nsteps_1000000_x0_[0.01, 0]_u0_[0, 0]'
+    directory8 = 'raw/wf_N_plus_sin_ff_linear_dt_1e-07_nsteps_1000000_x0_[0.01, 0]_u0_[0, 0]'
+    directory9 = 'raw/wf_one_plus_sin_ff_linear_offset_dt_1e-07_nsteps_1000000_x0_[0.01, 0]_u0_[0, 0]'
     #load_res = data.load_instance(directory,filename)
     #data.sinlge_plot(directory1,'particle_data_rhoP800_dP1e-06_V010000.0_T0.000644946677103762_phi0_wf_one_plus_sin_ff_linear.pkl')
     #data.plot_trajectory(directory1,'particle_data_rhoP800_dP1e-06_V010000.0_T0.000644946677103762_phi0_wf_one_plus_sin_ff_linear.pkl')
@@ -62,12 +64,12 @@ if __name__ == '__main__':
     variable_dict = {
         'rhoP': [800,2000],
         'dP': [1e-6],
-        'V0': [1e4]
+        'V0': [1000]
     }
-    #data.separation_over_time(directory3,variable_dict,T_plot_list=[2e-4],minus_DC=True,normalizeDC=True,comp = False)
-    #data.separation_rate_over_time(directory3,variable_dict,T_plot_list=[2e-4],minus_DC=True,normalizeDC=True,comp = False)
-    data.energy_over_time(directory3,variable_dict,T_plot_list=[2e-4],minus_DC=True,normalizeDC=True,comp = False)
-    #data.compare_final_x_minus_dc(directory3,variable_dict,minus_DC=True,normalizeDC=True,comp = False)
-    #data.compare_final_energy(directory7,variable_dict,minus_DC=True,normalizeDC=True,comp = False)
+    #data.separation_over_time(directory8,variable_dict,T_plot_list=[2e-4],minus_DC=True,normalizeDC=True,comp = False)
+    #data.separation_rate_over_time(directory8,variable_dict,T_plot_list=[2e-4],minus_DC=True,normalizeDC=True,comp = False)
+    #data.energy_over_time(directory8,variable_dict,T_plot_list=[2e-4],minus_DC=True,normalizeDC=True,comp = False)
+    data.compare_final_x_minus_dc(directory9,variable_dict,minus_DC=True,normalizeDC=True,comp = False)
+    #data.compare_final_energy(directory8,variable_dict,minus_DC=True,normalizeDC=True,comp = False)
     print('time',duration)
     #print(res)