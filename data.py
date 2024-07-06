fixed mistake in normalizing. previous calculations in r is wrong since it was normalized by coordinates. instead, it should be normalized by displacement.

data.py
@@ -283,6 +283,7 @@ def compare_final_x_minus_dc(directory, variable_dict, minus_DC = True, normaliz
 
     plt.tight_layout()
     plt.show()
+
 def filter_filenames_by_variables(directory, variable_dict,compressed):
     matching_files = []
     for filename in os.listdir(directory):
@@ -367,8 +368,7 @@ def find_closest(array, value):
     differences = np.abs(array - value)
     index = np.argmin(differences)
     return array[index]
-## NOT DONE YET
-def separation_over_time(directory, variable_dict,T_plot_list, minus_DC = True, normalizeDC = True, comp = False):
+def separation_over_time(directory, variable_dict,T_plot_list, minus_DC = True, normalizeDC = True, comp = False, fitting = False):
     fig, axs = plt.subplots(2, 1, figsize=(10, 8))
 
     keys, values = zip(*variable_dict.items())
@@ -395,12 +395,55 @@ def separation_over_time(directory, variable_dict,T_plot_list, minus_DC = True, 
         DC_params['T'] = 0.0
         DCdata = find_DC_file_with_parameters(directory, DC_params, compressed=comp)
         for T in T_plot_list:
-            for i in range(0,len(data)):
-                if data[i].T <= T < data[i + 1].T:
-                    axs[0].plot(data[i].time_points,data[i].trajectory[:,0]/DCdata.trajectory[:,0]-1,label = label)
-                    axs[1].plot(data[i].time_points, data[i].trajectory[:, 1]/DCdata.trajectory[:,0]-1, label= label)
+            diff = []
+            for i in range(0, len(data)):
+                diff.append(abs(data[i].T - T))
+            iplot = np.argmin(diff)
+            closestT = data[iplot].T
+            time_points = data[iplot].time_points
+            r_plot=[]
+            z_plot=[]
+            if minus_DC:
+                if normalizeDC:
+                    r_plot = (data[iplot].trajectory[:, 0]-x0[0]) / (DCdata.trajectory[:, 0]-x0[0]) - 1
+                    z_plot = (data[iplot].trajectory[:, 1]-x0[1]) / (DCdata.trajectory[:, 1]-x0[1]) - 1
+                else:
+                    r_plot = (data[iplot].trajectory[:, 0]) - (DCdata.trajectory[:, 0])
+                    z_plot = (data[iplot].trajectory[:, 1]) - (DCdata.trajectory[:, 1])
+            else:
+                if normalizeDC:
+                    r_plot = (data[iplot].trajectory[:, 0] - x0[0]) / (DCdata.trajectory[:, 0] - x0[0])
+                    z_plot = (data[iplot].trajectory[:, 1] - x0[1]) / (DCdata.trajectory[:, 1] - x0[1])
+                else:
+                    r_plot = (data[iplot].trajectory[:, 0])
+                    z_plot = (data[iplot].trajectory[:, 1])
 
-                    break
+            axs[0].plot(time_points[100000:],
+                        r_plot[100000:]
+                        , label=label)
+            axs[1].plot(time_points[100000:],
+                        z_plot[100000:]
+                        , label=label)
+            if fitting:
+                r_fit_func, r_coeff = fit_polynomial_fraction(time_points,
+                                                              r_plot,
+                                                         poly_degree=1, frac_range=(1 / 8, 1))
+
+                z_fit_func, z_coeff = fit_polynomial_fraction(time_points,
+                                                              z_plot,
+                                                         poly_degree=1, frac_range=(1 / 8, 1))
+
+                t_fit = np.linspace(min(data[iplot].time_points), max(data[iplot].time_points), 1000)
+                r_poly_fit = r_fit_func(t_fit)
+                z_poly_fit = z_fit_func(t_fit)
+
+                axs[0].plot(t_fit,r_poly_fit,label = f"r_fitted_coeff = {r_coeff} ")
+                axs[1].plot(t_fit, z_poly_fit, label=f"z_fitted_coeff = {z_coeff} ")
+                print(f"r_fitted_coeff = {r_coeff}")
+                print(f"z_fitted_coeff = {z_coeff}")
+
+            print('plot for T =', closestT)
+
     axs[0].set_xlabel('t')
     axs[0].set_ylabel('r')
     axs[0].autoscale()
@@ -411,6 +454,21 @@ def separation_over_time(directory, variable_dict,T_plot_list, minus_DC = True, 
 
     plt.tight_layout()
     plt.show()
+
+def fit_polynomial_fraction(t,x, poly_degree=2, frac_range=(1 / 8, 7 / 8)):
+    # Determine the start and end indices based on the fraction range
+    start_idx = int(len(x) * frac_range[0])
+    end_idx = int(len(x) * frac_range[1])
+
+    # Filter the data
+    x_filtered = x[start_idx:end_idx:5]
+    t_filtered = t[start_idx:end_idx:5]
+
+    # Polynomial fit
+    poly_coeffs = np.polyfit(t_filtered, x_filtered, poly_degree)
+    poly_fit = np.poly1d(poly_coeffs)
+
+    return poly_fit, poly_coeffs
 def separation_rate_over_time(directory, variable_dict,T_plot_list, minus_DC = True, normalizeDC = True, comp = False):
     fig, axs = plt.subplots(2, 1, figsize=(10, 8))
 

functions.py
@@ -1,9 +1,79 @@
 import math
+from scipy.ndimage import gaussian_filter1d
+import matplotlib.pyplot as plt
 import numpy as np
 from functools import partial
 from scipy.integrate import quad
 
 # waveforms are defined here
+# waveforms should be in the format of:
+# base_waveform, (optionally smoothed waveform) and fast computing version using interpolation instead of calculating
+
+
+class InterpolatingFunction:
+    def __init__(self, array_producing_function, x_values):
+        """
+        Initialize the InterpolatingFunction with a function to produce the array and x-values.
+
+        Parameters:
+        - array_producing_function: A function that takes x_values and produces an array.
+        - x_values: An array of x-values for interpolation.
+        """
+        self.array_producing_function = array_producing_function
+        self.x_values = x_values
+        self.array = None  # The array will be produced and stored on the first call
+        self.__name__ = array_producing_function.__name__[len("generate_"):]
+
+    def __call__(self, V0,T,t,phi):
+        """
+        Interpolate the value at a specific x using the provided parameters.
+
+        Parameters:
+        - x: The x-value for which to get the interpolated magnitude.
+        - params: Additional parameters to be applied to the interpolated value.
+
+        Returns:
+        - interpolated_value: The interpolated magnitude at the specified x.
+        """
+        if self.array is None:
+            # First call: produce and store the array
+            self.array = self.array_producing_function(self.x_values)
+        if T != 0:
+            phase = (t%T + phi)%1
+
+            # Interpolate using the stored array
+            interpolated_value = np.interp(phase, self.x_values, self.array)
+
+            interpolated_value *= V0
+            return interpolated_value
+        else:
+            return V0
+
+# Example array producing function
+def generate_smoothed_sqaure_wave(x_values):
+    a = 0.9
+    sigma = 100
+    pos_constant = (1) / a+1
+    neg_constant = (1) / (a - 1)+1
+
+    # Initialize the waveform array
+    waveform = np.zeros_like(x_values)
+
+    # Assign values based on the interval
+    waveform[x_values < a] = pos_constant
+    waveform[x_values >= a] = neg_constant
+    extended_waveform = np.concatenate((waveform, waveform, waveform))
+    smoothed_waveform = gaussian_filter1d(extended_waveform, sigma, mode='reflect')
+    smoothed_waveform = np.array(smoothed_waveform)
+    smoothed_waveform = smoothed_waveform[len(waveform):2 * len(waveform)]
+    # Adjust to preserve the mean
+    mean_original = np.mean(waveform)
+    mean_smoothed = np.mean(smoothed_waveform)
+    smoothed_waveform += mean_original - mean_smoothed
+    return smoothed_waveform
+
+x_values = np.linspace(0,1,10000)
+smoothed_square_wave = InterpolatingFunction(generate_smoothed_sqaure_wave, x_values)
 
 def DC(V0,T,t,phi):
     return -V0
@@ -32,10 +102,10 @@ def _05_plus_sin(V0,T,t,phi):
     else:
         return -V0
 def N_plus_sin(V0,T,t,phi):
-    N = 3
+    N = 1000
     if T != 0:
         phase = 2 * math.pi * t / T + phi
-        return -V0*(1+(1/N)*np.sin(phase))
+        return -V0*(1+N*np.sin(phase))
     else:
         return -V0
 
@@ -52,31 +122,66 @@ def half_sawtooth(V0,T,t,phi):
     if T != 0:
         # increase exponentially to a certain value then decrease exponentially
         # the rate of increase and decrease are determined by the coeffs
-        a = 1 # coeff of charging
+        a = 5 # coeff of charging
         b = 1 # coeff of discharging
-        c = 1 # scaling coeff, used to have time average value of 1
+        c = 1/0.48933336199819516# scaling coeff, used to have time average value of 1
         phase = (2 * math.pi * (t % T) / T + phi) % (2*math.pi)/(2*math.pi)
         # note that phase is defined different here for the function to be defined between 0 and 1
-        return (min(np.exp(a*phase),np.exp(b*(1-phase)))-1)*c*V0
+        return -(min(np.exp(a*phase),np.exp(b*(1-phase)))-1)*c*V0
     else:
         return -V0
 
-def half_sawtooth_calc(t):
-    V0 = 1
-    T = 1
-    phi = 0
-    a = 1 # coeff of charging
-    b = 1 # coeff of discharging
-    c = 1/0.29744254140025633 # scaling coeff, used to have time average value of 1
-    phase = (2 * math.pi * (t % T) / T + phi) % (2*math.pi)/(2*math.pi)
-    # note that phase is defined different here for the function to be defined between 0 and 1
-    return (min(np.exp(a*phase),np.exp(b*(1-phase)))-1)*c*V0
-    return t
+def smoothed_half_sawtooth(V0,T,t,phi):
+    window_size = 0.1
+    # window size measured in the fraction of time period it is averaging over.
+    sample_N = 5
+    # total number of samples to be taken to find the average, should be an odd number to include the central point
+    sample_step = window_size/(sample_N-1)
+    sum = 0
+    for i in range(0,sample_N):
+        sum += half_sawtooth(V0,T,t+T*(-sample_step*(sample_N-1)/2+i*sample_step),phi)
+    return sum/sample_N
+
+# smoothed sawtooth function is rather computationally expensive, since it essentially computes half sawtooth 'sample_N' times.
+# A better solution might be to have a pre-computed list ( within one cycle) as a look up table.
+
+partial_smooth_half_sawtooth = partial(smoothed_half_sawtooth,V0=1,T=1,phi=0)
 def calculate_para_half_sawtooth():
-    result = quad(half_sawtooth_calc,0,1)
+    result = quad(partial_smooth_half_sawtooth,0,1)
     print(result)
 
 #calculate_para_half_sawtooth()
+
+# This is the code to speed up the half sawtooth calculation by calculating the list to interpolate the first time this function is called.
+# The parameters are calculated in the first run as well
+
+def square_wave(V0,T,t,phi):
+    a = 0.9
+    # a is the fraction of period that the function is positive. (voltage is negative)
+    if T != 0 :
+        phase = ((t % T) / T + phi / (2 * math.pi)) % 1
+        if phase <= a:
+            return -V0/(2*a-1)
+        else:
+            return V0/(2*a-1)
+    else:
+        return -V0
+
+
+
+def plot_waveform(waveform):
+    t_arr = np.linspace(0,3,5000)
+    V = []
+    V0 = 1
+    T = 1
+    phi = 0
+    for t in t_arr:
+        V.append(waveform(V0,T,t,phi))
+
+    plt.plot(t_arr,V)
+    plt.show()
+
+#plot_waveform(smoothed_square_wave)
 
 # flowfields are defined here:
 
@@ -100,10 +205,14 @@ waveform_functions = {
     'DC': DC,
     'one_plus_sin': one_plus_sin,
     'half_sawtooth': half_sawtooth,
+    'smoothed_half_sawtooth': smoothed_half_sawtooth,
     '_075_plus_sin': _075_plus_sin,
     '_05_plus_sin': _05_plus_sin,
-    'abs_sine' : abs_sine,
-    'N_plus_sin': N_plus_sin
+    'abs_sine': abs_sine,
+    'N_plus_sin': N_plus_sin,
+    'square_wave': square_wave,
+    'smoothed_square_wave' : smoothed_square_wave
+
 }
 
 flowfield_functions = {

main.py
@@ -13,7 +13,7 @@ parameters1 = {
     "phi" : [0],
     "V0" : [1000],
     #"T" : np.linspace(1e-4, 10e-4, 100).tolist()
-    "T" : sorted(np.logspace(-6, -3, num=7, base=10).tolist() + [0.00])
+    "T" : sorted(np.logspace(-6, -3, num=8, base=10).tolist() + [0.00])
 }
 
 parameters2 = {
@@ -25,8 +25,8 @@ parameters2 = {
 }
 
 
-waveform = functions.one_plus_sin
-flowfield = functions.linear_offset
+waveform = functions.smoothed_square_wave
+flowfield = functions.linear
 dt = 1e-7
 n_steps = int(1e6)
 # 40s for one particle running 1e6 steps
@@ -44,17 +44,18 @@ if __name__ == '__main__':
 
     # run solver with parameter, ff and wf
     start_time = time.time()
-    #solver.sweep(parameters1,simulate_single_phi_avg_partial)
+    solver.sweep(parameters1,simulate_single_phi_avg_partial)
     duration = time.time() - start_time
 
 
     directory3 = 'raw/wf_one_plus_sin_ff_linear_dt_1e-07_nsteps_1000000_x0_[0.01, 0]_u0_[0, 0]'
     directory4 = 'raw/wf__075_plus_sin_ff_linear_dt_1e-07_nsteps_1000000_x0_[0.01, 0]_u0_[0, 0]'
-    directory5 = 'raw/wf_half_sawtooth_ff_linear_dt_1e-07_nsteps_1000000_x0_[0.01, 0]_u0_[0, 0]'
+    directory5 = 'raw/wf_smoothed_half_sawtooth_ff_linear_dt_1e-07_nsteps_1000000_x0_[0.01, 0]_u0_[0, 0]'
     directory6 = 'raw/wf__05_plus_sin_ff_linear_dt_1e-07_nsteps_1000000_x0_[0.01, 0]_u0_[0, 0]'
-    directory7 = 'raw/wf_N_plus_sin_ff_linear_dt_1e-07_nsteps_1000000_x0_[0.01, 0]_u0_[0, 0]'
     directory8 = 'raw/wf_N_plus_sin_ff_linear_dt_1e-07_nsteps_1000000_x0_[0.01, 0]_u0_[0, 0]'
     directory9 = 'raw/wf_one_plus_sin_ff_linear_offset_dt_1e-07_nsteps_1000000_x0_[0.01, 0]_u0_[0, 0]'
+    directory10 = 'raw/wf_smoothed_half_sawtooth_ff_linear_dt_1e-07_nsteps_1000000_x0_[0.01, 0]_u0_[0, 0]'
+    directory11 = 'raw/wf_square_wave_ff_linear_dt_1e-07_nsteps_1000000_x0_[0.01, 0]_u0_[0, 0]'
     #load_res = data.load_instance(directory,filename)
     #data.sinlge_plot(directory1,'particle_data_rhoP800_dP1e-06_V010000.0_T0.000644946677103762_phi0_wf_one_plus_sin_ff_linear.pkl')
     #data.plot_trajectory(directory1,'particle_data_rhoP800_dP1e-06_V010000.0_T0.000644946677103762_phi0_wf_one_plus_sin_ff_linear.pkl')
@@ -66,10 +67,10 @@ if __name__ == '__main__':
         'dP': [1e-6],
         'V0': [1000]
     }
-    #data.separation_over_time(directory8,variable_dict,T_plot_list=[2e-4],minus_DC=True,normalizeDC=True,comp = False)
+    #data.separation_over_time(directory11,variable_dict,T_plot_list=[1e-4],minus_DC=True,normalizeDC=False,comp = False,fitting=False)
     #data.separation_rate_over_time(directory8,variable_dict,T_plot_list=[2e-4],minus_DC=True,normalizeDC=True,comp = False)
     #data.energy_over_time(directory8,variable_dict,T_plot_list=[2e-4],minus_DC=True,normalizeDC=True,comp = False)
-    data.compare_final_x_minus_dc(directory9,variable_dict,minus_DC=True,normalizeDC=True,comp = False)
+    data.compare_final_x_minus_dc(directory11,variable_dict,minus_DC=True,normalizeDC=True,comp = False)
     #data.compare_final_energy(directory8,variable_dict,minus_DC=True,normalizeDC=True,comp = False)
     print('time',duration)
     #print(res)